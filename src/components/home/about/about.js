4e4e96c4076c1a0111abde0c28bbd6e0f7392635
Sergio Rocha <sergio@Sergios-MacBook-Pro.local>
2019-09-30 17:39:15 -0500

    Adding all the nchanges

--- src/components/home/about/about.js
+++ src/components/home/about/about.js
@@ -1,4 +1,7 @@
 import React from "react";
+import reactPic from "./react.jpg";
+import nodePic from "./node.png";
+import frontendPic from "./frontend.png";
 
 const About = () => {
   return (
@@ -9,12 +12,14 @@ const About = () => {
       <div class="row">
         <div class="col-1-of-2">
           <h3 class="tertiary-heading utility-margin-bottom-small">
-            JavaScript Geek
+            Coding Aficionado, Pro Gamer, Dad
           </h3>
           <p class="paragraph">
-            I love javascript! So naturally I tend to gravitate towards JS
-            frameworks and libraries but I do work with Ruby based technologies
-            as well. I am a full stack web developer with a passion for making
+            Just a couple things I love<br></br>
+            Coding!<br></br>
+            Competing in fighting games!<br></br>
+            Being a dad!<br></br>
+            I am a full stack web developer with a passion for making
             things both beautiful and efficient. I am known for throwing myself
             into an environment and getting things done, whether it requires
             diving in headfirst to learn a new framework/technology, to leading
@@ -22,9 +27,8 @@ const About = () => {
             important elements for success. I use my keen sense of focus to
             solve problems that come my way. As a previous pro level video game
             competitor, I found that my focus was what separated myself from the
-            many other great players and lead me to all my tournament wins. Now
-            my focus is web development and I am excited to see what we can
-            accomplish.
+            many other great players and lead me to all my tournament wins. My current
+            passion in the web and creating content for it!
           </p>
           <h3 class="tertiary-heading utility-margin-bottom-small">100% CSS</h3>
           <p class="paragraph">
@@ -44,17 +48,17 @@ const About = () => {
             <img
               alt="Photo Google"
               class="composition__photo composition__photo--1"
-              src="img/react.jpg"
+              src={reactPic}
             />
             <img
               alt="Photo SEO"
               class="composition__photo composition__photo--3"
-              src="img/frontend.png"
+              src={frontendPic}
             />
             <img
               alt="Photo Gizmos"
               class="composition__photo composition__photo--2"
-              src="img/node.png"
+              src={nodePic}
             />
           </div>
         </div>
