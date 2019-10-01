d07f8159e645d972d8d2639d7d0b3a92081b8289
Sergio Rocha <sergio@Sergios-MacBook-Pro.local>
2019-09-30 22:20:42 -0500

    Removed image and page 2 link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,10 @@ const IndexPage = () => (
     <Navigation/>
     <AboutSection/>
     <InformationSection/>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    </div> */}
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
